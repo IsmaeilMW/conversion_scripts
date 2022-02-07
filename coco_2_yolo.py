6d90dae8b29898040f5e186672623092b806ad24
ismaeilVLSI <--unset-all>
2022-02-07 07:08:22 +0530

    update for coco_2_yolo.py.

--- coco_2_yolo.py
+++ coco_2_yolo.py
@@ -6,11 +6,40 @@ __author__ = "Mohammad Ismaeil"
 import os
 from pycocotools.coco import COCO
 import shutil
+import cv2
+import tqdm
 
 
-def coco2yolo(cat_names, ann_files, save_dir):
+def resize_image(img_file, height_width):
+    resized_img = cv2.resize(img_file, (height_width[1], height_width[0]))
+    return resized_img
+
+
+def resize_annotation_data(ann_bbox, r_height_width, o_height_width):
+    r_h, r_w = r_height_width
+    o_h, o_w = o_height_width
+    h_f, w_f = r_h / o_h, r_w / o_w
+    bbox = ann_bbox[0] * w_f, ann_bbox[1] * h_f, ann_bbox[2] * w_f, ann_bbox[3] * h_f
+    return bbox
+
+
+def copy_files(source_file, des_file):
+    shutil.copy2(source_file, des_file)
+
+
+def coco2yolo(cat_names, ann_files, img_files, r_height_width, save_dir):
     # initialize COCO api for instance annotations
+    resize_flag = False
     coco = COCO(ann_files)
+
+    "Adding connected and disconnected on suffix of the event labels"
+    cat_names_s = []
+    for label_name in cat_names:
+        if label_name in event_cat:
+            cat_names_s.append(label_name + "_connected")
+            cat_names_s.append(label_name + "_disconnected")
+        else:
+            cat_names_s.append(label_name)
 
     # Create an index for the category names
     cats = coco.loadCats(coco.getCatIds())
@@ -21,7 +50,7 @@ def coco2yolo(cat_names, ann_files, save_dir):
     for img in coco.imgs:
 
         # Get all annotation IDs for the image
-        cat_ids = coco.getCatIds(catNms=cat_names)
+        cat_ids = coco.getCatIds(catNms=cat_names_s)
         ann_ids = coco.getAnnIds(imgIds=[img], catIds=cat_ids)
 
         # If there are annotations, create a label file
@@ -30,16 +59,33 @@ def coco2yolo(cat_names, ann_files, save_dir):
             img_fname = coco.imgs[img]['file_name']
             width = coco.imgs[img]['width']
             height = coco.imgs[img]['height']
+            o_height_width = [height, width]
+
+            "check whether resize is required"
+            if o_height_width[0] == height:
+                copy_files(img_files + '/' + img_fname, save_dir + '/' + img_fname)
+            else:
+                resize_flag = True
+                img = cv2.imread(img_files + '/' + img_fname)
+                resize_img = resize_image(img, r_height_width)
+                cv2.imwrite(save_dir + '/' + img_fname, resize_img)
+
             # open text file
             with open(save_dir + '/' + img_fname.split('.')[0] + '.txt', 'w') as label_file:
                 anns = coco.loadAnns(ann_ids)
                 for a in anns:
                     bbox = a['bbox']
+                    if resize_flag:
+                        bbox = resize_annotation_data(bbox, r_height_width, o_height_width)
+
                     # Convert COCO bbox coords to yolo ones
                     centre_x, centre_y = bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2
-                    bbox = [centre_x/width, centre_y/height, bbox[2]/width, bbox[3]/height]
+                    bbox = [centre_x / width, centre_y / height, bbox[2] / width, bbox[3] / height]
                     bbox = [str(b) for b in bbox]
                     cat_name = cat_idx[a['category_id']]
+                    split_cat_name = cat_name.split('_')
+                    if split_cat_name[-1] in ['connected', 'disconnected']:
+                        cat_name = '_'.join(split_cat_name[:-1])
                     cat_id = sel_catNms.index(cat_name)
                     # Format line in label file
                     # Note: all whitespace will be removed from class names
@@ -53,6 +99,8 @@ if __name__ == '__main__':
     os.chdir(r"..\\")
     dataset_list = ['train', 'test', 'val']
     output_dir = os.getcwd() + '/model_data/datasets/1/detection/yolo/'
+    req_height = 720
+    req_width = 1280
 
     # Check if old file exits.
     if os.path.exists(output_dir):
@@ -64,26 +112,19 @@ if __name__ == '__main__':
     else:
         os.mkdir(output_dir)
 
-    # If this list is populated then label files will only be produced
-    # for images containing the listed classes and only the listed classes
-    # will be in the label file
-    # catNms = ['person', 'dog', 'skateboard']
-    # All cat names = ["cargo_loader", "jet_bridge", "belt_loader",
-    #               "catering_vehicle", "cargo_door_opener_ladder", "pca", "airplane",
-    #               "aircraft_front", "pushback_tug", "baggage", "tow_tractor", "chocks_on", "baggage_trailor",
-    #               "chocks_off", "belt_loader_version2", "fwd_cargo_door_open"]
+    event_cat = ["cargo_loader", "jet_bridge", "belt_loader", "catering_vehicle", "cargo_door_opener_ladder",
+                 "pca", "pushback_tug"]
+    non_event_cat = ["airplane", "aircraft_front", "pushback_tug", "baggage", "tow_tractor", "chocks_on",
+                     "baggage_trailor", "chocks_off", "belt_loader_version2", "fwd_cargo_door_open"]
     sel_catNms = ["cargo_loader", "jet_bridge", "belt_loader",
                   "catering_vehicle", "pca",
                   "aircraft_front", "pushback_tug"]
 
-    for d_set in dataset_list:
-        # These settings assume this script is in the annotations directory
-        # dataDir = 'model_data/image_w_ann/train'
-        # segmentType = 'train'
-        # annFile = '%s/annotations/instances_%s.json' % (dataDir, segmentType)
-        annFile = 'model_data/datasets/1/detection/coco/%s/dataset.json' % d_set
+    for d_set in tqdm.tqdm(dataset_list):
+        annFile = os.getcwd() + '/model_data/datasets/1/coco/%s/dataset.json' % d_set
+        imgFile = os.getcwd() + '/model_data/datasets/1/' + d_set
         if os.path.isdir(output_dir + d_set):
             print('Labels folder already exists - exiting to prevent badness')
         else:
             os.mkdir(output_dir + d_set)
-            coco2yolo(sel_catNms, annFile, output_dir + d_set)
+            coco2yolo(sel_catNms, annFile, imgFile, [req_height, req_width], output_dir + d_set)
