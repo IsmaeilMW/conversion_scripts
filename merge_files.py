d2e54f0f1c9d1de2dae704cf17dad64346efeaf9
ismaeilVLSI <--unset-all>
2022-06-08 18:30:57 +0530

    updated with pathlib.

--- merge_files.py
+++ merge_files.py
@@ -1,20 +1,22 @@
 import os
 import shutil
 import tqdm
+import yaml
+from pathlib import Path
 
-os.chdir("..\\")
+# os.chdir("..\\")
 
 
 def create_dir(save_dir):
     if os.path.exists(save_dir):
         shutil.rmtree(save_dir)
         os.mkdir(save_dir)
-        os.mkdir(save_dir + '/' + 'images')
-        os.mkdir(save_dir + '/' + 'annotation')
+        os.mkdir(save_dir / 'images')
+        os.mkdir(save_dir / 'annotation')
     else:
         os.mkdir(save_dir)
-        os.mkdir(save_dir + '/' + 'images')
-        os.mkdir(save_dir + '/' + 'annotation')
+        os.mkdir(save_dir / 'images')
+        os.mkdir(save_dir / 'annotation')
 
 
 def copy_files(source_file, des_file):
@@ -41,28 +43,32 @@ def merge_files(img_dir, ann_dir, save_dir):
         else:
             img_dir_name = ann_dir_name
             dir_id = split_dir_name[-1]
-        ann_file_list = os.listdir(ann_dir + '/' + ann_dir_name)
+        ann_file_list = os.listdir(ann_dir / ann_dir_name)
         for ann_file_name in ann_file_list:
             current_file_id = ann_file_name.split('.')[0]
             img_file_name = current_file_id + '.jpg'
-            src_img = img_dir + '/' + img_dir_name + '/' + img_file_name
-            src_ann = ann_dir + '/' + ann_dir_name + '/' + ann_file_name
+            src_img = img_dir / img_dir_name / img_file_name
+            src_ann = ann_dir / ann_dir_name / ann_file_name
             if "_" in current_file_id:
-                # copy_files(src_img, save_dir + '/' + 'images/' + img_file_name)
-                # copy_files(src_ann, save_dir + '/' + 'annotation/' + ann_file_name)
-                move_files(src_img, save_dir + '/' + 'images/' + img_file_name)
-                move_files(src_ann, save_dir + '/' + 'annotation/' + ann_file_name)
+                # copy_files(src_img, save_dir / 'images/' + img_file_name)
+                # copy_files(src_ann, save_dir / 'annotation/' + ann_file_name)
+                move_files(src_img, save_dir / 'images' / img_file_name)
+                move_files(src_ann, save_dir / 'annotation' / ann_file_name)
             else:
                 new_file_id = 'v' + dir_id + '_' + current_file_id
                 img_file_name = new_file_id + '.jpg'
                 ann_file_name = new_file_id + '.json'
-                rename_and_copy_files(src_img, save_dir + '/' + 'images/' + img_file_name)
-                rename_and_copy_files(src_ann, save_dir + '/' + 'annotation/' + ann_file_name)
+                rename_and_copy_files(src_img, save_dir / 'images' / img_file_name)
+                rename_and_copy_files(src_ann, save_dir / 'annotation' / ann_file_name)
 
 
 if __name__ == '__main__':
-    dataset_dir = '5'
-    root_dir = os.getcwd() + '/model_data/datasets/' + dataset_dir
-    output_dir = root_dir + '/merge_files'
+    with open('params.yaml') as f:
+        my_dict = yaml.safe_load(f)
+    FILE = Path(__file__).resolve()
+    ROOT = FILE.parents[1]
+    dataset_dir = my_dict['training']['save_dir']
+    root_dir = ROOT / dataset_dir
+    output_dir = root_dir / 'merge_files'
     create_dir(output_dir)
-    merge_files(root_dir + '/images', root_dir + '/annotation', output_dir)
+    merge_files(root_dir / 'images', root_dir / 'annotation', output_dir)
--- split_test_train_val.py
+++ split_test_train_val.py
@@ -2,6 +2,8 @@ import json
 import os
 import shutil
 import tqdm
+import yaml
+from pathlib import Path
 
 
 def copy_data(input_loc, output_loc, json_or_img):
@@ -18,20 +20,20 @@ def move_files(source_file, des_file):
 def change_path_in_json(json_path, save_dir):
     with open(json_path, 'r') as json_file:
         json_data = json.load(json_file)
-        file_name = json_path.split('/')[-1]
+        file_name = Path(json_path).parts[-1]
         img_path = file_name.split('.')[0] + '.jpg'
         json_data['imagePath'] = img_path
 
         if 'imageData' in json_data:
             json_data['imageData'] = None
-    with open(save_dir + '/' + file_name, 'w') as save_file:
+    with open(save_dir / file_name, 'w') as save_file:
         json.dump(json_data, save_file)
 
 
 def create_directory(save_dir):
     if os.path.exists(save_dir):
-        # shutil.rmtree(save_dir + '/' + 'video_1_modified')
-        # os.mkdir(save_dir + '/' + 'video_1_modified')
+        # shutil.rmtree(save_dir / 'video_1_modified')
+        # os.mkdir(save_dir / 'video_1_modified')
         pass
     else:
         os.mkdir(save_dir)
@@ -44,7 +46,7 @@ def create_test_train_val(in_image_dir, in_annotation_dir, save_dir):
     test_count = 0
 
     for dir_name in ['test', 'train', 'val']:
-        create_directory(save_dir + '/' + dir_name)
+        create_directory(save_dir / dir_name)
 
     for file_name in tqdm.tqdm(file_list):
         split_file_name = file_name.split('.')
@@ -52,35 +54,36 @@ def create_test_train_val(in_image_dir, in_annotation_dir, save_dir):
         if count % 10 == 0:
             if val_count >= test_count:
                 # pass to validation
-                move_files(in_image_dir + '/' + file_name, save_dir + 'test' + '/' + file_name)
-                copy_data(in_annotation_dir + '/' + ann_file_name, save_dir + 'test', 'json')
+                move_files(in_image_dir / file_name, save_dir / 'test' / file_name)
+                copy_data(in_annotation_dir / ann_file_name, save_dir / 'test', 'json')
 
-                # copy_data(in_image_dir + '/' + file_name, save_dir + 'test', 'img')
+                # copy_data(in_image_dir / file_name, save_dir + 'test', 'img')
                 test_count += 1
             elif test_count > val_count:
-                move_files(in_image_dir + '/' + file_name, save_dir + 'val' + '/' + file_name)
-                copy_data(in_annotation_dir + '/' + ann_file_name, save_dir + 'val', 'json')
+                move_files(in_image_dir / file_name, save_dir / 'val' / file_name)
+                copy_data(in_annotation_dir / ann_file_name, save_dir / 'val', 'json')
 
-                # copy_data(in_image_dir + '/' + file_name, save_dir + 'val', 'img')
+                # copy_data(in_image_dir / file_name, save_dir + 'val', 'img')
                 val_count += 1
 
         else:
-            move_files(in_image_dir + '/' + file_name, save_dir + 'train' '/' + file_name)
-            copy_data(in_annotation_dir + '/' + ann_file_name, save_dir + 'train', 'json')
+            move_files(in_image_dir / file_name, save_dir / 'train' / file_name)
+            copy_data(in_annotation_dir / ann_file_name, save_dir / 'train', 'json')
 
-            # copy_data(in_image_dir + '/' + file_name, save_dir + 'train', 'img')
+            # copy_data(in_image_dir / file_name, save_dir + 'train', 'img')
 
         count += 1
 
 
 if __name__ == '__main__':
-    if os.name == 'nt':
-        os.chdir(r"..\\")
-    else:
-        os.chdir(r"../")
-    dataset_dir = '5'
+    with open('params.yaml') as f:
+        my_dict = yaml.safe_load(f)
+    FILE = Path(__file__).resolve()
+    ROOT = FILE.parents[1]
+    dataset_dir = my_dict['training']['save_dir']
+    root_dir = ROOT / dataset_dir
 
-    input_image_dir = os.getcwd() + '/model_data/datasets/' + dataset_dir + '/merge_files/images'
-    input_annotation_dir = os.getcwd() + '/model_data/datasets/' + dataset_dir + '/merge_files/annotation'
-    output_dir = os.getcwd() + '/model_data/datasets/' + dataset_dir + '/'
+    input_image_dir = root_dir / 'merge_files/images'
+    input_annotation_dir = root_dir / 'merge_files/annotation'
+    output_dir = root_dir
     create_test_train_val(input_image_dir, input_annotation_dir, output_dir)
